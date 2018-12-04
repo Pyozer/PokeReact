55073bb00766dca5dfb5d105e77a024485ee4fcf
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-12-04 23:16:33 +0100

    Split row into a component

--- pokedex-client/src/components/PokemonInfosCard.js
+++ pokedex-client/src/components/PokemonInfosCard.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import '../css/PokemonInfosCard.css'
 
+const PokemonInfosCardRow = ({ title, value }) => (
+    <div className="row py-row mb-0">
+        <div className="col s6 xl5 truncate capitalize">{title}</div>
+        <div className="col s6 xl7 truncate capitalize"><strong>{value}</strong></div>
+    </div>
+)
+
 const PokemonInfosCard = ({ data }) => {
     let tableLines = []
     Object.entries(data).filter(([k, v]) => v).forEach(([key, value]) => {
-        tableLines.push(
-            <div className="row py-row mb-0" key={key}>
-                <div className="col s6 xl5 truncate capitalize">{key}</div>
-                <div className="col s6 xl7 truncate capitalize"><strong>{value}</strong></div>
-            </div>
-        )
+        tableLines.push(<PokemonInfosCardRow title={key} value={value} />)
     })
 
     if (tableLines.length === 0) return <></>
